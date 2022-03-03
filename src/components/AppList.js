96a3fd7fd71175d0b9c348c7bee52abc382422f0
Prashant Vekariya <prashantvekariya321@gmail.com>
2022-03-03 12:21:42 +0530

    changes

--- src/components/AppList.js
+++ src/components/AppList.js
@@ -4,7 +4,6 @@ import { BiCode } from "react-icons/bi";
 import { RiBug2Fill } from "react-icons/ri";
 import { MdFace } from "react-icons/md";
 
-
 const AppList = ({ data }) => {
   return (
     <div className="d-flex border-bottom p-1 align-items-center">
@@ -13,10 +12,7 @@ const AppList = ({ data }) => {
       </div>
       <div className="text-break">
         <h5 className="m-0 p-0">{data.name}</h5>
-        <small className="text-secondary">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-          Exercitationem, repellendus.
-        </small>
+        <small className="text-secondary">{data.description}</small>
         <div className="text-secondary">
           {data.language && (
             <span>
